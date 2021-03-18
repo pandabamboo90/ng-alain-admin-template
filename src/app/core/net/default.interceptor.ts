0e89dac76b68e63cb11b9f13d4f7d1281a1e796c
Dang Nguyen <pandabamboo90@gmail.com>
2021-03-18 23:01:59 +0700

    feat: change error message

--- src/app/core/net/default.interceptor.ts
+++ src/app/core/net/default.interceptor.ts
@@ -18,25 +18,25 @@ import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
 import { catchError, filter, mergeMap, switchMap, take } from 'rxjs/operators';
 
 const CODEMESSAGE: { [key: number]: string } = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
+  200: 'Success',
+  201: 'Created',
+  202: 'A request has entered the background queue (asynchronous task).',
+  204: 'Operation succeed',
+  400: 'Bad request',
+  401: 'Unauthenticated',
+  403: 'Permission denied',
+  404: 'Not found',
+  406: 'The requested format is not available',
+  410: 'The requested resource is permanently deleted and will no longer be available',
+  422: 'Validation error',
+  500: 'Internal server error',
+  502: 'Bad gateway',
+  503: 'The service is unavailable, and the server is temporarily overloaded or maintained',
+  504: 'Gateway timeout。',
 };
 
 /**
- * 默认HTTP拦截器，其注册细节见 `app.module.ts`
+ * The default HTTP interceptor, please refer to `app.module.ts` for registration details
  */
 @Injectable()
 export class DefaultInterceptor implements HttpInterceptor {
