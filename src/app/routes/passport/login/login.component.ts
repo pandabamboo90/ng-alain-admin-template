9b3467e1b1fea7aa63f2cb61e0d3f546ce2095dd
Dang Nguyen <pandabamboo90@gmail.com>
2021-03-20 17:29:56 +0700

    feat: finish logic for login form

--- src/app/routes/passport/login/login.component.ts
+++ src/app/routes/passport/login/login.component.ts
@@ -1,9 +1,10 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { StartupService } from '@core';
-import { DA_SERVICE_TOKEN, ITokenService, SocialService } from '@delon/auth';
-import { _HttpClient, SettingsService } from '@delon/theme';
+import { DA_SERVICE_TOKEN, ITokenModel, ITokenService, SocialService } from '@delon/auth';
+import { SettingsService } from '@delon/theme';
 import { FormlyFieldConfig } from '@ngx-formly/core/lib/components/formly.field.config';
 
 @Component({
@@ -42,23 +43,39 @@ export class UserLoginComponent implements OnInit {
   constructor(
     fb: FormBuilder,
     private router: Router,
-    public http: _HttpClient,
+    public http: HttpClient,
     private settingsService: SettingsService,
     @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
     private startupSrv: StartupService,
   ) {
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   submit(): void {
     this.http
-      .post('/login/account?_allow_anonymous=true', {
-        userName: this.form.value.email,
+      .post('/auth_admin/sign_in?_allow_anonymous=true', {
+        email: this.form.value.email,
         password: this.form.value.password,
-      })
-      .subscribe((res) => {
-        this.tokenService.set(res.user);
+      }, { observe: 'response' })
+      .subscribe((res: HttpResponse<any>) => {
+        const credentials = JSON.stringify({
+          accessToken: res.headers.get('access-token') || '',
+          tokenType: res.headers.get('token-type') || '',
+          expiry: res.headers.get('expiry') || '',
+          client: res.headers.get('client') || '',
+          uid: res.headers.get('uid') || '',
+        });
+
+        const token: ITokenModel = {
+          token: credentials,
+          // expired: +new Date() + 1000 * 60 * 5
+          expired: parseInt(res.headers.get('expiry') || '0', 10) * 1000,
+        };
+        this.tokenService.set(token);
+
+        // Get the StartupService content, we always believe that the application information will generally be affected by the scope of authorization of the current user
         this.startupSrv.load().then(() => {
           let url = this.tokenService.referrer!.url || '/';
           if (url.includes('/passport')) {
